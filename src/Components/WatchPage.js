14665cf5614768899bd1b4ffca11da9d2d63749b
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2025-06-15 12:56:15 +0530

    UI: Responsive design for Watch page

--- src/Components/WatchPage.js
+++ src/Components/WatchPage.js
@@ -33,17 +33,17 @@ const WatchPage = () => {
 
 
   return (
-    <div className='pl-12 flex flex-col w-full'>
-      <div className='flex w-full'>
+    <div className='pl-1 pr-1 sm:pr-0 sm:pl-12 flex flex-col w-full'>
+      <div className='flex flex-col sm:flex-row w-full'>
         <div>
-        <iframe width="800" height="420" className='rounded-xl mr-4' src={"https://www.youtube.com/embed/"+ VideoKey} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <iframe height="420" className='rounded-xl mr-4 w-full sm:w-[800px] mb-2 sm:mb-0' src={"https://www.youtube.com/embed/"+ VideoKey} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
         <div className='w-full mr-4'>
           <LiveChat/>
         </div>
       </div>
-      <div className='flex'>
-        <div className='w-[800px]'><CommentsContainer/></div>
+      <div className='flex flex-col sm:flex-row'>
+        <div className='w-full sm:w-[800px]'><CommentsContainer/></div>
         <div className='mx-4'>
           <h3 className='text-center font-bold text-lg my-4'>Recommended Videos</h3>
           <div className='max-w-[380px]'>
